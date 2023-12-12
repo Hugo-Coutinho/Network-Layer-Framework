ae0c9c10ff4c7372986b571cbb25db0d0fce2c21
Hugo Coutinho <>
2023-12-12 17:45:47 +0100

    first commit

---
+++ Sources/Network/BaseRequestInput.swift
@@ -0,0 +1,16 @@
+//
+//  BaseRequestInput.swift
+//  SpaceX
+//
+//  Created by hugo.coutinho on 16/12/21.
+//  Copyright © 2021 . All rights reserved.
+//
+
+import Foundation
+import Combine
+
+public protocol BaseRequestInput: AnyObject {
+    // MARK: - INPUT METHODS -
+    func doRequest(urlString: String, completionHandler: @escaping (Data?) -> Void)
+    func fetchAnyPublisherWith(_ url: URL) -> AnyPublisher<Data, APIError>
+}
